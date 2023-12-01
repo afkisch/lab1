01e226d73343af85b22e4af6e76124d5e152a032
afkisch <104489999+afkisch@users.noreply.github.com>
2023-12-01 01:38:42 +0100

    Update clk_fall_rise.v

--- clk_fall_rise.v
+++ clk_fall_rise.v
@@ -1,33 +1,31 @@
 `timescale 1ns / 1ps
 
-module clk(
-	input clk,
-	input rst,
-	input [3:0] clksel,
+module clk_div(
+	input clk, rst, freq,
 	output reg clk_out,
-	output clk_out_f,
-	output clk_out_r
+	output clk_out_r,
+	output clk_out_f
 );
 
 reg [2:0] cntr;
+wire [1:0] cmp;
 
 always @(posedge clk)
-	begin
 	if(rst)
 		begin
 			cntr <= 3'b0;
 			clk_out <= 1'b0;
 		end
-	else
-		cntr <= cntr + 3'b1;
-	if(cntr == clksel-1)
+	else if(cntr == cmp)
 		begin
-		clk_out <= ~clk_out;
-		cntr <= 3'b0;
+			clk_out <= ~clk_out;
+			cntr <= 3'b0;
 		end
-	end
+	else
+		cntr <= cntr + 1'b1;
 
-assign clk_out_f = (cntr == clksel-1) && (clk_out == 1);
-assign clk_out_r = (cntr == clksel-1) && (clk_out == 0);
+assign cmp = (freq == 0) ? 2'b01 : 2'b11;
+assign clk_out_r = (cntr == cmp) && (clk_out == 0);
+assign clk_out_f = (cntr == cmp) && (clk_out == 1);
 
 endmodule
